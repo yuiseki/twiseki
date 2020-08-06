eff892949d737d620b26921e04e654c8d374ddbb
yuiseki <yuiseki@gmail.com>
2020-08-06 16:06:22 +0900

    コマンドラインを修正

--- index.js
+++ index.js
@@ -2,40 +2,21 @@
 const user = require('./user');
 const search = require('./search');
 
-async function searching(args){
-  const tweets = await search.getSearchResult(args);
-  for (const id in tweets) {
-    if (tweets.hasOwnProperty(id)) {
-      const tweet = tweets[id];
-      console.log(tweet.created_at);
-      console.log('\t'+tweet.full_text.replace(/\r?\n/g,' '));
-    }
-  }
-}
-
-async function timeline(args){
-  const profile = await user.getProfile(args);
-  const tweets = await user.getTimeline(args);
-  for (const id in tweets) {
-    if (tweets.hasOwnProperty(id)) {
-      const tweet = tweets[id];
-      if(tweet.user_id_str === profile.user_id_str){
-        console.log(tweet.created_at);
-        console.log('\t'+tweet.full_text.replace(/\r?\n/g,' '));
-      }
-    }
-  }
-}
-
 !(async() => {
   const command_type = process.argv[2];
   const command_args = process.argv[3];
   switch (command_type) {
+    case 'profile':
+      const profile = await user.getProfile(command_args);
+      console.log(profile);
+      break
     case 'timeline':
-      await timeline(command_args);
+      const timeline = await user.getTimeline(command_args);
+      console.log(timeline);
       break;
     case 'search':
-      await searching(command_args);
+      const searchResult = await search.getSearchResult(command_args);
+      console.log(searchResult);
       break;
     default:
       break;
